4e733a7401aa4ec40344fe16bfc4d412c6edd21e
Kenny Ogbogu <ogbogukenny@yahoo.com>
2025-06-21 17:55:44 +0100

    Update PropertyBulletin.tsx
    Added Nosa's email to admin access

--- src/components/PropertyBulletin.tsx
+++ src/components/PropertyBulletin.tsx
@@ -38,7 +38,7 @@ const PropertyBulletin = () => {
   const cloudinaryConfig = { cloudName: cloudName, uploadPreset: uploadPreset }
 
   // Admin email constant
-  const ADMIN_EMAIL = useMemo(() => (["ogbogukenny@yahoo.com", "nelshantel101@gmail.com"]), []);
+  const ADMIN_EMAIL = useMemo(() => (["ogbogukenny@yahoo.com", "nelshantel101@gmail.com", "anthonynosa111@gmail.com"]), []);
 
   const { 
     headerTitle, 
