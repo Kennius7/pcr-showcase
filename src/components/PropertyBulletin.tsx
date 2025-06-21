ef3290ce929305da73c68f672082b05083f125e6
Kenny Ogbogu <ogbogukenny@yahoo.com>
2025-06-21 14:44:55 +0100

    Update PropertyBulletin.tsx
    change of admin email array

--- src/components/PropertyBulletin.tsx
+++ src/components/PropertyBulletin.tsx
@@ -38,7 +38,7 @@ const PropertyBulletin = () => {
   const cloudinaryConfig = { cloudName: cloudName, uploadPreset: uploadPreset }
 
   // Admin email constant
-  const ADMIN_EMAIL = useMemo(() => (["ogbogukenny@yahoo.com", "shinjinchu@gmail.com"]), []);
+  const ADMIN_EMAIL = useMemo(() => (["ogbogukenny@yahoo.com"]), []);
 
   const { 
     headerTitle, 
